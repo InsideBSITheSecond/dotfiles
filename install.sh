e1a5313be5211dea0f207ab014e793c77ff6877c
InsideBSITheSecond <insidebsiprod@gmail.com>
2025-05-16 00:36:30 +0200

    (installer) pacman --noconfirm

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Needed programs
-sudo pacman -S fish waybar rofi nvim hyprpaper nemo keepassxc \
+sudo pacman -S --noconfirm fish waybar rofi nvim hyprpaper nemo keepassxc \
     cmake meson cpio pkg-config git g++ gcc
 
 # Hyprland plugins:
