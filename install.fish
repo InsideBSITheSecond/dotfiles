3d0dd97dde3fd53d04f6d2d993839f00c495dc5a
InsideBSITheSecond <insidebsiprod@gmail.com>
2025-05-16 00:53:23 +0200

    (installer) moving everything but fish install in another .fish script

---
+++ install.fish
@@ -0,0 +1,31 @@
+#!/bin/fish
+sudo pacman -S --noconfirm --needed waybar rofi nvim hyprpaper nemo keepassxc \
+    cmake meson cpio pkg-config git g++ gcc base-devel
+
+# - (yay)
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+makepkg -si
+
+# Hyprshade
+yay -S hyprshade
+
+# Hyprland plugins:
+# - (hyprpm)
+hyprpm update
+hyprpm add https://github.com/shezdy/hyprsplit
+hyprpm enable hyprsplit
+
+#Fisher, Fish plugin manager:
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+#Fisher plugins:
+fisher install IlanCosman/tide@v5
+fisher install jethrokuan/z
+fisher install patrickf1/fzf.fish
+fisher install franciscolourenco/done
+fisher install joseluisq/gitnow@2.12.0
+fisher install paldepind/projectdo
+
+# Tide configure
+tide configure
--- install.sh
+++ install.sh
@@ -1,27 +1,10 @@
 #!/bin/sh
 
-# Needed programs
-sudo pacman -S --noconfirm fish waybar rofi nvim hyprpaper nemo keepassxc \
-    cmake meson cpio pkg-config git g++ gcc
+# Install fish
+sudo pacman -S --noconfirm --needed fish
 
-# Hyprland plugins:
-# - (hyprpm)
-hyprpm add https://github.com/shezdy/hyprsplit
-hyprpm enable hyprsplit
+# Make it default
+chsh -s $(which fish)
 
-# - (yay)
-yay -S hyprshade
-
-#Fisher, Fish plugin manager:
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-#Fisher plugins:
-fisher install IlanCosman/tide@v5
-fisher install jethrokuan/z
-fisher install patrickf1/fzf.fish
-fisher install franciscolourenco/done
-fisher install joseluisq/gitnow@2.12.0
-fisher install paldepind/projectdo
-
-# Tide configure
-tide configure
+# Start install
+fish install.fish
