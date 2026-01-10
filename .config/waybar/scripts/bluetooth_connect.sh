d421481f1c744913c9cad1cb513f3e36dfd1d90b
InsideBSITheSecond <insidebsiprod@gmail.com>
2026-01-10 23:39:36 +0100

    (bluetooth) changes default mac address

--- .config/waybar/scripts/bluetooth_connect.sh
+++ .config/waybar/scripts/bluetooth_connect.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/expect -f
 
-set device "11:11:22:F3:6C:EC"
+#set device "11:11:22:F3:6C:EC"
+set device "91:7F:3B:C9:89:5D"
 set timeout 60
 spawn bluetoothctl
 send -- "scan on\r"
 expect "$device"
-send -- "pair $device\r"
-expect "Pairing successful"
+#send -- "pair $device\r"
+#expect "Pairing successful"
 send -- "connect $device\r"
 expect "Connection successful"
 send -- "trust $device\r"
